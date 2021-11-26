ffd10fcabedc9e3b4429f7bfc2c7fd4e32900c39
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2021-11-27 00:32:08 +0530

    tightly coupled example

--- Simple-gaming-app/src/main/java/com/in28minutes/Simple/gaming/app/SimpleGamingAppApplication.java
+++ Simple-gaming-app/src/main/java/com/in28minutes/Simple/gaming/app/SimpleGamingAppApplication.java
@@ -3,11 +3,20 @@ package com.in28minutes.Simple.gaming.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.in28minutes.Simple.gaming.app.game.GameRunner;
+import com.in28minutes.Simple.gaming.app.game.MarioGame;
+
 @SpringBootApplication
 public class SimpleGamingAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleGamingAppApplication.class, args);
+		//SpringApplication.run(SimpleGamingAppApplication.class, args);
+		
+		MarioGame game = new MarioGame();
+		
+		GameRunner runner = new GameRunner(game);
+		
+		runner.runGame();
 	}
 
 }
