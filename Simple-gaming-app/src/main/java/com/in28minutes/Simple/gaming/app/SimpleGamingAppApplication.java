8c2c98210d7925a06d14b0932108e30bc86547e3
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2021-11-26 23:50:19 +0530

    learning spring framework

---
+++ Simple-gaming-app/src/main/java/com/in28minutes/Simple/gaming/app/SimpleGamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.Simple.gaming.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGamingAppApplication.class, args);
+	}
+
+}
---
+++ Simple-gaming-app/src/test/java/com/in28minutes/Simple/gaming/app/SimpleGamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.Simple.gaming.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGamingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
