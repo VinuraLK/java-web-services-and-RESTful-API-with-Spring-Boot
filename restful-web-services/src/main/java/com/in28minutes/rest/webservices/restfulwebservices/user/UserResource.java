39df7d8e34420af02ea5bc3c17b029dfc2d2f2dc
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2021-12-22 02:37:10 +0530

    Implementing Exception Handling

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -25,7 +25,10 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user==null)
+			throw new UserNotFoundException("ïd-"+id)
+		return user;
 	}
 	
 	@PostMapping("/users")
