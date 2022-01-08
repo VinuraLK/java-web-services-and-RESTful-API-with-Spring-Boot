26dd8a7cd4367ae430dc5cc38455611cbee793e3
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2022-01-09 00:00:01 +0530

    Introduction to Swagger

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.context.annotation.Configuration;
+
+//configaration
+@Configuration
+public class SwaggerConfig {
+
+}
