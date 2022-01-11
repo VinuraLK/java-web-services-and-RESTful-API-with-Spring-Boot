93c8c754ee2f069ff428868225bfc1261c120a2f
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2022-01-11 23:59:47 +0530

    Enhancing Swagger Documentation with custom Annotations

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,8 +18,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	public static final Contact DEFAULT_CONTACT= new Contact("","","");
+	public static final ApiInfo DEFAULT_API_INFO= new ApiInfo( "Api Documentation","Api Documentation","1.0","urn:tos",
+    DEFAULT_CONTACT,
+    "Apache 2.0",
+    "http://www.apache.org/licenses/LICENSE-2.0",
+    new ArrayList<>());
+	
+	private static final ApiInfo DEFAULT_API_INFO = null;
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO);
 	}
 }
