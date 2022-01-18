20e0fefa6667c3c2b47dafa24b6a617d174d04fb
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2022-01-19 00:00:04 +0530

    Implementing Dynamic Filtering for the Restful service

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,18 +1,35 @@
 package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 	
+	//field1,Field2
 	@GetMapping("/filtering")
 	public SomeBean retrieveSomeBean() {
-		return new SomeBean("values1","value 2","value 3");
+		SomeBean someBean = new SomeBean("value1","value 2","value 3");
+		
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFIlter", filter);
+		
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		mapping.setFilters(filters);
+		
+		return  someBean;
 		}
 	
+	//field2,field3
 	@GetMapping("/filtering-list")
 	public List<SomeBean> retrieveListOfSomeBeans() {
-		return Arrays.aslist (new SomeBean("values1","value 2","value 3"),
-				new Somebean("value12","value22","value33");
+		return Arrays.asList(new SomeBean("values1","value 2","value 3"),
+				new SomeBean("value12","value22","value32"));
+}
 }
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,12 +1,16 @@
 package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 
 public class SomeBean {
 	private String field1;
+	
+
 	private String field2;
 	
-	@JsonIgnore
+	
 	private String field3;
 	public SomeBean(String field1, String field2, String field3) {
 		super();
